a39f8259367b3cf221aa9e8e0bd5ec2d54e45716
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-25 16:29:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function add(a, b) {
+ console.log('add a, b')
+ return 770 + 1
+}
+
+function subtract(a, b) {
+
+}
+
+function multiply(a, b) {
+
+}
+
+function divide(a, b) {
+
+}
+
+function inc(n) {
+
+}
+
+function dec(n) {
+
+}
+
+function makeInt(n) {
+
+}
+
+function preserveDecimal(n){
+
+}
