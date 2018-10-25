29d10dd6bac86c95ab2bc97bee590816c8f3395a
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-25 16:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-
+ console.log('multiply a,b')
+ return a * b
 }
 
 function divide(a, b) {
-
+  console.log('divide a,b')
+  return a / b
 }
 
 function inc(n) {
